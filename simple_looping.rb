bbe3119902db065203d686f95e9d0e794c5790fd
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-25 01:46:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,10 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
-    
+    counter += 1
+  puts "Iteration #{counter} of the loop"
+  if counter >= 10 
+    break
     end
   end
 
