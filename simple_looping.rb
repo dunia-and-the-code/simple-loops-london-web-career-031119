5685862338f897b21be7d2050f2996ea69dc070c
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-25 01:24:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,8 +9,8 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   7.times do
-    break
     puts "Welcome to Flatiron School's Web Development Course!"
+    break
 end
 
 def times_iterator(number_of_times)
