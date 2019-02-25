c1deb1a672a3b4915f1dc9092c8d1b9a9285712d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-25 01:46:26 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,8 +12,8 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     counter += 1
-  puts "Iteration #{counter} of the loop"
-  if counter >= 10 
+  puts phrase
+  if counter >= 7 
     break
     end
   end
