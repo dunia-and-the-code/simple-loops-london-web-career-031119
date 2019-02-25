5f0e06004b3b2f5cc179714e00ab13210c6accd7
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-25 02:14:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,8 +36,8 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 1..7
-  for number in counter
+  number_of_times = 1..7
+  for number in number_of_times
     puts phrase
   end
 end
